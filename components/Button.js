e1dd92c3ee1406b958967c3be27ae692796a2d32
ctripleu <cuoyemike@gmail.com>
2021-05-22 04:07:16 +0100

    Added button component

---
+++ components/Button.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Button = (props) => {
+
+    return (
+        <div>
+            <button
+            style={{ backgroundColor: props.backgroundColor, margin:"10px" }}
+            className="btn btn-lg"
+            >
+                {props.title}
+            </button>
+        </div>
+    )
+}
+
+export default Button;
